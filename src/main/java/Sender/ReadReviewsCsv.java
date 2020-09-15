f5c3d96e5211a54d339491b2a85ed25009846d61
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-09-15 12:37:53 +0530

    Adding Columnfilter function

--- src/main/java/Sender/ReadReviewsCsv.java
+++ src/main/java/Sender/ReadReviewsCsv.java
@@ -1,17 +1,8 @@
-package Sender;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ReadReviewsCsv {
-	public static List<String> reviewCsvReader(String filename, String col) throws IOException{
-		List<String>  Reviews=new ArrayList<String>();
-		String row="";
-		BufferedReader csvReader=new BufferedReader(new FileReader(filename));
-		String firstrow=csvReader.readLine();
+	
+	public static Integer Columnfilter(String firstrow, String col) {
 		String[] columns=firstrow.split(",");
 		Integer primecolumn=(Integer) null; 
 		for(Integer i=0;i<columns.length;i++)
@@ -22,8 +13,17 @@ public class ReadReviewsCsv {
 				break;
 			}
 		}
+		return primecolumn;
 		
-		  
+	}
+	public static List<String> reviewCsvReader(String filename, String col) throws IOException{
+		List<String>  Reviews=new ArrayList<String>();
+		String row="";
+		BufferedReader csvReader=new BufferedReader(new FileReader(filename));
+		String firstrow=csvReader.readLine();
+		
+		Integer primecolumn=Columnfilter(firstrow, col);
+		
 		while((row = csvReader.readLine())!=null) {
 			String[] data=row.split(",");
 			if(data.length>1)
@@ -37,5 +37,7 @@ public class ReadReviewsCsv {
 		return Reviews;
 	}
 
+	
+	
 
 }
