ac8d2d0f4bca6b9664ff6692fe3af3d92039d5e6
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-09-15 12:46:55 +0530

    Update Main.java

--- src/main/java/Sender/Main.java
+++ src/main/java/Sender/Main.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
+	
+	public static void WriteReviews(List<String> reviews) {
+		ConsoleWriter.writeReviewsOnConsole(reviews);
+	}
 	public static void main(String[] args) throws IOException{
 		
 		
@@ -10,7 +14,7 @@ public class Main {
 		
 		reviewsList = ReadReviewsCsv.reviewCsvReader(FileFetcher.readCsvfileNameFromProperties(), args[0]);
 		
-		ConsoleWriter.writeReviewsOnConsole(reviewsList);
+		WriteReviews(reviewsList);
 		
 	}
 
