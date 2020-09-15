0b4ece283cdb387e1cb1de265a7c80bbc9543465
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-09-15 12:20:58 +0530

    Accomodated the arguments change inside reviewCsvReader
    Added facility for column filter

--- src/main/java/Sender/ReadReviewsCsv.java
+++ src/main/java/Sender/ReadReviewsCsv.java
@@ -1,19 +1,31 @@
-package Sender;
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReadReviewsCsv {
-	public static List<String> reviewCsvReader(String filename) throws IOException{
+	public static List<String> reviewCsvReader(String filename, String col) throws IOException{
 		List<String>  Reviews=new ArrayList<String>();
 		String row="";
 		BufferedReader csvReader=new BufferedReader(new FileReader(filename));
+		String firstrow=csvReader.readLine();
+		String[] columns=firstrow.split(",");
+		Integer primecolumn=(Integer) null; 
+		for(Integer i=0;i<columns.length;i++)
+		{
+			if(columns[i].equals(col))
+			{
+				primecolumn=i;
+				break;
+			}
+		}
+		
+		  
 		while((row = csvReader.readLine())!=null) {
 			String[] data=row.split(",");
 			if(data.length>1)
-				Reviews.add(data[1]);
+				Reviews.add(data[primecolumn]);
+			
 			/*for(String s:data) {
 				Reviews.add(s);
 			}*/
@@ -22,4 +34,4 @@ public class ReadReviewsCsv {
 		return Reviews;
 	}
 
-}
+	
