5adcfc5a74139726dcd9e3c648c900da3b15101d
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-09-22 08:35:59 +0530

    updated Main

--- src/main/java/Sender/Main.java
+++ src/main/java/Sender/Main.java
@@ -1,20 +1,28 @@
 package Sender;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
 	public static boolean isMainFunctionCalled=false;
 	 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		
 	
 		List<String> reviewsList = null;
 		
 		try {
 			reviewsList = ReadReviewsCsv.reviewCsvFile_ParsesLinesAndAddsToList(FileFetcher.readCsvfileNameFromProperties(), args[0]);
-		} catch (Exception e) {
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		catch(IOException f) {
+			f.printStackTrace();
+		}
+		catch(ColumnNameNotFoundException col)
+		{
+			col.printStackTrace();
 		}
 		
 		ConsoleWriter.writeReviewsOnConsole(reviewsList);
